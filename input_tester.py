52661fbb1b937ad84b340d2e784f11e332b4eff9
ronan <g00342393@gmit.ie>
2019-02-10 21:47:12 +0000

    Added NFA class and improved input loop and commenting

--- input_tester.py
+++ input_tester.py
@@ -1,4 +1,16 @@
+import sys
 
+
+class NFA:
+    """class representing a non-deterministic finite automaton"""
+
+    def __init__(self, regex):
+        self.alphabet = set(regex)
+        print("Alphabet: ", self.alphabet)
+
+
+# print the welcome text block
+# TODO: read this from a file instead?
 print("\n== Regex NFA Builder by Ronan Hanley ==\n")
 
 print("Set the regular expression by typing regex=(regex here)")
@@ -8,18 +20,33 @@ print("You can give input text to test against that regex by just typing it in b
 
 # regular expression string to compare against provided input
 regex = None
-line_read = None
+# last line of user input read from the command line
+line_read = ""
 
-while line_read != "exit":
+# continuously parse and process user input
+while True:
+    # read in line of user input
     line_read = input("> ")
+    # make a lowercase copy of the input for case insensitive comparisons
+    line_read_lower = line_read.lower()
 
-    if line_read.startswith("regex="):
+    if line_read_lower == "exit":
+        # exit the program
+        print("\nExiting...")
+        sys.exit()
+
+    if line_read_lower.startswith("regex="):
+        # user wants to set the regex to a string they've provided
         regex = line_read[6:]
         print("New regex string:", regex)
+        NFA(regex)
     else:
+        # assume the user intends to test this entered string against the regex
         if regex is None:
+            # regex has not yet been set
             print("Please supply a regular expression string first")
         else:
-            print("(Matching test done here)")
+            print("(Regex matching test done here)")
 
+    # print a new line for aesthetics
     print()

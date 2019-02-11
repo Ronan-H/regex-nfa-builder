e7adc53edc1c2ea3581b819795a1cbf634e34964
ronan <g00342393@gmit.ie>
2019-02-11 22:54:37 +0000

    Added more state and behaviour to the NFA class

--- input_tester.py
+++ input_tester.py
@@ -5,8 +5,45 @@ class NFA:
     """class representing a non-deterministic finite automaton"""
 
     def __init__(self, regex):
+        self.states = set()
         self.alphabet = set(regex)
+        self.transition_function = {}
+        self.start_state = 0
+        self.accept_states = set()
+
+        # set of states that the NFA is currently in
+        self.in_states = set()
+
         print("Alphabet: ", self.alphabet)
+
+    def add_state(self, state):
+        self.states += state
+
+    def add_transition(self, from_state, symbol, to_state):
+        self.transition_function[(from_state, symbol)] = to_state
+
+    def feed_symbol(self, symbol):
+        # feeds a symbol into the NFA, calculating which states the
+        # NFA is now in, based on which states it used to be in
+
+        new_states = set()
+
+        # process each old state in turn
+        for state in self.states:
+            pair = (state, symbol)
+
+            # check for a legal transition from the old state to a
+            # new state, based on what symbol was fed in
+            if pair in self.states:
+                # add the corresponding new state to the updated states list
+                new_states.add(self.transition_function[pair])
+
+        self.states = new_states
+
+    def is_accepting(self):
+        # accepts if we are in ANY accept states
+        # ie. accepts if in_states and accept_states share any states in common
+        return len(self.in_states & self.accept_states) > 0
 
 
 # print the welcome text block
